690c147be639873c2f3dc33a9c817df9cf35222e
Sojeong Park <macpark09@gmail.com>
2021-06-25 00:36:39 +0100

    add coffee.js

---
+++ db/data/coffee.js
@@ -0,0 +1,73 @@
+module.exports = [
+    {
+        drink:'Latte',
+        description:'Latte is comprised of a shot of espresso and steamed milk ',
+        img_url:'https://images.unsplash.com/photo-1529892485617-25f63cd7b1e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80',
+        calories:157,
+        type:'mild'
+    },
+    {
+        drink:'Cappuccino',
+        description:'Cappuccino is comprised of a shot of espresso and more foam than steamed milk',
+        img_url:'https://images.unsplash.com/photo-1553381006-e9b84d5952c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80',
+        calories:157,
+        type:'mild'
+    },
+    {
+        drink:'Mocha',
+        description:'Mocha is comprised of a shot of espresso , steamed milk and chocolate sauce',
+        img_url:'https://images.unsplash.com/photo-1569564358029-a3f834632c6a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
+        calories:200,
+        type:'sweet'
+    },
+    {
+        drink:'Americano',
+        description:'Americano is a shot of espresso diluted in hot water',
+        img_url:'https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
+        calories:5,
+        type:'bitter'
+    },
+    {
+        drink:'Cortado',
+        description:'Cortado is a shot of espresso and less steamed milk',
+        img_url:'https://images.unsplash.com/photo-1514066558159-fc8c737ef259?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
+        calories:100,
+        type:'bitter   '
+    
+    },
+    {
+        drink:'Flat white',
+        description:'Flat white is an espresso drink with steamed milk.',
+        img_url:'https://images.unsplash.com/photo-1502462041640-b3d7e50d0662?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80',
+        calories:100,
+        type:'mild'
+
+    },
+    {
+        drink:'Espresso',
+        description:'only espresso shot',
+        img_url:'https://images.unsplash.com/photo-1428550443830-190057dc8098?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80',
+        calories:5,
+        type:'bitter'
+
+    },
+    
+    {
+        drink:'Caramel macchiato',
+        description:'Caramel macchiato is comprised of a shot of espresso and steamed milk with vanilla syrup and caramel drizzle. ',
+        img_url:'https://images.unsplash.com/photo-1592318951566-70f4518609a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
+        calories:250,
+        type:'sweet'
+
+    },
+    {
+        drink:'Hazelnut latte',
+        description:'Hazelnut latte is comprised of a shot of espresso and steamed milk with hazelnut syrup',
+        img_url:'https://images.unsplash.com/photo-1606821466874-1e3eb48c9442?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
+        calories:200,
+        type:'sweet'
+
+    }
+    
+    
+]
